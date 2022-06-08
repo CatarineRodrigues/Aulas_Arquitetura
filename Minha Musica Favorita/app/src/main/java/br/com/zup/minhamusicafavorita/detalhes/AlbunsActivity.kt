23fba771b3a3bc1344aa3df926dbd55b9a19f800
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-06-08 15:46:57 -0300

    Criar nova activity de detalhes do albun

---
+++ Minha Musica Favorita/app/src/main/java/br/com/zup/minhamusicafavorita/detalhes/AlbunsActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.minhamusicafavorita.detalhes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.minhamusicafavorita.R
+
+class AlbunsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_albuns)
+    }
+}
