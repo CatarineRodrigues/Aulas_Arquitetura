1de683724bf61e0c39cb02ad78a253d5af201117
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-06-08 20:43:19 -0300

    Criar layout item

--- Minha Musica Favorita/app/src/main/java/br/com/zup/minhamusicafavorita/detalhes/albuns/AlbunsActivity.kt
+++ Minha Musica Favorita/app/src/main/java/br/com/zup/minhamusicafavorita/detalhes/albuns/AlbunsActivity.kt
@@ -2,11 +2,29 @@ package br.com.zup.minhamusicafavorita.detalhes.albuns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import br.com.zup.minhamusicafavorita.R
+import br.com.zup.minhamusicafavorita.databinding.ActivityAlbunsBinding
 
 class AlbunsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAlbunsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_albuns)
+        binding = ActivityAlbunsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        acessarActionBar()
+    }
+
+    private fun acessarActionBar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setTitle(R.string.detalhe_da_banda)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
