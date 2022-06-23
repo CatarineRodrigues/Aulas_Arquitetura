deafe5cdf565ecbc1ad462d3ea25f544279faef0
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-06-22 21:22:33 -0300

    Criar class RegisterModel

---
+++ Exercicio_MovieFlix/MovieFlix/app/src/main/java/br/com/zup/movieflix/register/model/RegisterModel.kt
@@ -0,0 +1,9 @@
+package br.com.zup.movieflix.register.model
+
+class RegisterModel(
+    var userName: String,
+    var userEmail: String,
+    var password: String,
+    var passwordConfirmation: String,
+) {
+}
